3b5cde0c3100d9b539f8681db1de320e17e7c57e
Jason Corcoran <jasoncorcoran0306@gmail.com>
2025-03-09 14:47:37 +1100

    fix: remove unused response fields from query

--- src/components/CharacterList.tsx
+++ src/components/CharacterList.tsx
@@ -253,10 +253,7 @@ export const GET_CHARACTERS = gql`
   query GetCharacters($page: Int) {
     characters(page: $page) {
       info {
-        count
         pages
-        next
-        prev
       }
       results {
         name
